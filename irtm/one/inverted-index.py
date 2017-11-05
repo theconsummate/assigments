a42b2febc4b023d1e1e64d4aa68766d1e4f6ca77
Dhruv Mishra <ubiquitiousdm@gmail.com>
2017-11-05 13:22:23 +0100

    adding comments

--- irtm/one/inverted-index.py
+++ irtm/one/inverted-index.py
@@ -7,11 +7,23 @@ import bisect
 
 
 logging.basicConfig(format='%(asctime)s [%(levelname)s]\t%(message)s', level=logging.DEBUG)
+# define postings list and dictionary variables
 dictionary = dict()
 posting_list = []
 
 
+"""
+This file implements an inverted index using a list of tweets as input corpus.
+The tweet id is used as a document id in the postings list.
+"""
+
+
 def index(filename):
+    """
+    implements an inverted index. Takes a valid file path as an input
+    Output: None. Initialized the global dictionary and postings_list variables.
+    """
+
     global dictionary, posting_list
     f = open(filename)
     for line in f.readlines():
@@ -19,10 +31,15 @@ def index(filename):
         if len(cols) < 5:
             # insufficient columns, skipping this document
             continue
+        # col[4] contains the tweet. Tokenise it on whitespaces and add to index
         for token in cols[4].split():
+            # toke is already present in dictionary.
+            # increase it's frequency by 1 and insert the tweet id in the postings list
             if token in dictionary:
                 dictionary[token]["size"] += 1
                 bisect.insort(posting_list[dictionary[token]["posting_list_index"]], cols[1])
+
+            # new token encountered. Insert it into our data structures
             else:
                 dictionary[token] = {"size": 1, "posting_list_index": len(posting_list)}
                 posting_list.append([])
@@ -32,6 +49,9 @@ def index(filename):
 
 
 def single_query(term):
+    """
+    returns the postings list for the query of a single term from the index
+    """
     if term in dictionary:
         return posting_list[dictionary[term]["posting_list_index"]]
     else:
@@ -39,6 +59,12 @@ def single_query(term):
 
 
 def query(term, *args):
+    """
+    Method overloading
+    calls the single_query method if only one term is provived.
+    If two terms are provided via the *args parameter,
+    the intersection of the postings list is returned.
+    """
     if len(args) == 0:
         return single_query(term)
     l1 = iter(query(term))
@@ -60,6 +86,9 @@ def query(term, *args):
         return l
 
 
+"""
+The program takes the input in the format python inverted-index.py <filename>
+"""
 if __name__ == '__main__':
     if len(sys.argv) > 2:
         logging.error("too many arguments provided")
@@ -67,7 +96,9 @@ if __name__ == '__main__':
         logging.error("too few arguments provided")
     else:
         logging.debug("input filename is " + sys.argv[1])
+        # check if the file exists at the given path
         if os.path.isfile(sys.argv[1]):
+            # create index by reading the given tweets file
             index(sys.argv[1])
             logging.debug(query("stuttgart"))
             logging.debug(query("bahn"))
