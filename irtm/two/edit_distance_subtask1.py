47ae8496fb0cee120626ab19537d5592f4e86c71
Dhruv Mishra <ubiquitiousdm@gmail.com>
2017-11-28 12:50:23 +0100

    fixed linting errors and added main method

--- irtm/two/edit_distance_subtask1.py
+++ irtm/two/edit_distance_subtask1.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import sys
 import logging
-import os.path
-import bisect
-#import pandas as pd
+import os
+# import bisect
+# import pandas as pd
 from nltk.metrics.distance import edit_distance
-import string
+# import string
 from collections import OrderedDict
 
 logging.basicConfig(format='%(asctime)s [%(levelname)s]\t%(message)s', level=logging.DEBUG)
@@ -15,17 +15,19 @@ posting_list = []
 fmap = dict()
 eng_dictionary = dict()
 
-def init_eng_dict(filename2):
+
+def init_eng_dict(english_words):
     global eng_dictionary
-    d = open(filename2)
+    d = open(english_words)
     for line in d.readlines():
         s = line.strip()
-        eng_dictionary[s.lower()] = {"size":0}
-    print len(eng_dictionary)
+        eng_dictionary[s.lower()] = {"size": 0}
+    print(len(eng_dictionary))
 
-def init_fmap(filename1):
+
+def init_fmap(tweets):
     global fmap, eng_dictionary
-    f = open(filename1)
+    f = open(tweets)
     for line in f.readlines():
         cols = line.strip().split("\t")
         if len(cols) < 5:
@@ -36,88 +38,56 @@ def init_fmap(filename1):
             # toke is already present in dictionary.
             # increase it's frequency by 1 and insert the tweet id in the postings list
             if token not in fmap:
-                fmap[token] = {"size" :1 , "isCorrect" : False}
-            else :
+                fmap[token] = {"size": 1, "isCorrect": False}
+            else:
                 fmap[token]["size"] += 1
-    #print len(fmap)
+    # print len(fmap)
+
 
 def minEditDistance(s1):
-    minimum = 20#sys.maxint
+    minimum = 20
+    # sys.maxint
     res = ""
     for token in eng_dictionary:
-        temp = edit_distance(s1.lower(),token.lower())
+        temp = edit_distance(s1.lower(), token.lower())
         if temp < minimum:
             minimum = temp
             res = token
-            break;
-    return minimum,res 
+            break
+    return minimum, res
 
-def onlytweet(filename1,filename2):
+
+def onlytweet(tweets, english_words):
     # df = pd.read_csv("tweets", sep="\t",names=['A','B','C','D','E'])
     # df = df[['E']]
     # print len(df)
     # df.to_csv("onlytweet")
     global fmap, eng_dictionary
-    init_eng_dict(filename2)
-    init_fmap(filename1)
+    init_eng_dict(english_words)
+    init_fmap(tweets)
     j = 0
     for token in fmap:
-        if (j%10==0):
-            print j
+        if (j % 10 == 0):
+            print(j)
         if token.lower() in eng_dictionary:
             fmap[token]["isCorrect"] = True
         else:
-            minimum,res = minEditDistance(token)
+            minimum, res = minEditDistance(token)
             # eng_dictionary[res]["size"] +=fmap[token]['size']
             fmap[token]["correct_term"] = res
             fmap[token]["edit_distance"] = minimum
-        j= j+1
-        #print token,fmap[token]
+        j = j + 1
+        # print token,fmap[token]
 
-    fmapSorted = OrderedDict(sorted(fmap.iteritems(), key=lambda x: x[1]['size'] , reverse=True))
+    fmapSorted = OrderedDict(sorted(fmap.iteritems(), key=lambda x: x[1]['size'], reverse=True))
     i = 0
-    for token in fmapSorted: 
-        if (i==10):
+    for token in fmapSorted:
+        if (i == 10):
             break
-        if fmap[token]["isCorrect"]==False:
-            i = i+1
-            print token,fmap[token]
+        if fmap[token]["isCorrect"] is False:
+            i = i + 1
+            print(token, fmap[token])
             break
-        
-"""
-This file implements an inverted index using a list of tweets as input corpus.
-The tweet id is used as a document id in the postings list.
-"""
-
-def index(filename):
-    """
-    implements an inverted index. Takes a valid file path as an input
-    Output: None. Initialized the global dictionary and postings_list variables.
-    """
-
-    global dictionary, posting_list
-    f = open(filename)
-    for line in f.readlines():
-        cols = line.strip().split("\t")
-        if len(cols) < 5:
-            # insufficient columns, skipping this document
-            continue
-        # col[4] contains the tweet. Tokenise it on whitespaces and add to index
-        for token in cols[4].split():
-            # toke is already present in dictionary.
-            # increase it's frequency by 1 and insert the tweet id in the postings list
-            if token in dictionary:
-                dictionary[token]["size"] += 1
-                bisect.insort(posting_list[dictionary[token]["posting_list_index"]],
-                              cols[1])
-
-            # new token encountered. Insert it into our data structures
-            else:
-                dictionary[token] = {"size": 1, "posting_list_index": len(posting_list)}
-                posting_list.append([])
-                posting_list[-1].append(cols[1])
-            # dictionary[token].append(cols[1])
-    return dictionary, posting_list
 
 
 def single_query(term):
@@ -157,24 +127,21 @@ def query(term, *args):
     except StopIteration as ex:
         return l
 
+
 # init_fmap("only")
-onlytweet("tweets","english-words")
-# """
-# The program takes the input in the format python inverted-index.py <filename>
-# """
-# if __name__ == '__main__':
-#     if len(sys.argv) > 2:
-#         logging.error("too many arguments provided")
-#     elif len(sys.argv) < 2:
-#         logging.error("too few arguments provided")
-#     else:
-#         logging.debug("input filename is " + sys.argv[1])
-#         # check if the file exists at the given path
-#         if os.path.isfile(sys.argv[1]):
-#             # create index by reading the given tweets file
-#             index(sys.argv[1])
-#             logging.debug(query("stuttgart"))
-#             logging.debug(query("bahn"))
-#             logging.debug(query("stuttgart", "bahn"))
-#         else:
-#             logging.error("input file is not present")
+"""
+The program takes the input in the format python inverted-index.py <filename>
+"""
+if __name__ == '__main__':
+    if len(sys.argv) > 3:
+        logging.error("too many arguments provided")
+    elif len(sys.argv) < 3:
+        logging.error("too few arguments provided")
+    else:
+        logging.debug("tweets filename is " + sys.argv[1])
+        logging.debug("english-words filename is " + sys.argv[2])
+        # check if the file exists at the given path
+        if os.path.isfile(sys.argv[1]) and os.path.isfile(sys.argv[2]):
+            onlytweet(sys.argv[1], sys.argv[2])
+        else:
+            logging.error("input file is not present")
